f9b2643b2bdb27bf85aad7603bda6824f1f072f8
CiSigEp <cisigep@gmail.com>
2019-06-15 10:26:34 -0500

    add event.js script tag

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,16 +1,13 @@
 
-function dateEvent(city, startDate, endDate, callback){
+function dateEvent(city, startDate, endDate, callback) {
 
-    var cityEvent=
-    $.get({
-        url: "https://www.eventbriteapi.com/v3/events/search/?q=" + city + "&token=TTEETSQICF2P3XU5IF2R&sort_by=date&start_date.range_start=" + startDate + "&start_date.range_end=" + endDate
-
-
-
-    }).done(callback)
-    .fail((err) => {
-        console.log("FAILED: " + err);
-    });
+    var cityEvent =
+        $.get({
+            url: "https://www.eventbriteapi.com/v3/events/search/?q=" + city + "&token=TTEETSQICF2P3XU5IF2R&sort_by=date&start_date.range_start=" + startDate + "&start_date.range_end=" + endDate
+        }).done(callback)
+            .fail((err) => {
+                console.log("FAILED: " + err);
+            });
 };
 
 // Make a variable that will hold the date inputs
